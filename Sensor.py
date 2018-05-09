89a814bcbeac1ae5f77442d938b1e40b1c162837
AetherialAvenger <37814541+AetherialAvenger@users.noreply.github.com>
2018-05-09 18:59:54 -0500

    Update Sensor.py
    MoistChk: returns High or Low depending on moisture of soil

--- Sensor.py
+++ Sensor.py
@@ -1,10 +1,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
-sensor = 5
+sensor = 20
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor, GPIO.IN)
 
 class Sensor(object):
     def MoistChk(self):
-        print not GPIO.input(sensor)
         return not GPIO.input(sensor)
